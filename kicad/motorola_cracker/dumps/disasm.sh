c4bfc8d4379d11d358703c1b78352e3457c87943
Tomas kovacik <nail@nodomain.sk>
2018-06-21 11:47:28 +0200

    no threads

--- kicad/motorola_cracker/dumps/disasm.sh
+++ kicad/motorola_cracker/dumps/disasm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+in=`echo $1|sed 's/\....//g'`
 #do not forget dasmx is win32, wine is required
 #./dasmx140/Dasmx.exe -a -c68HC05 -e0x400 -o0x400 -t $1
-./dasmx140/Dasmx.exe -a -t $1
+#./dasmx140/Dasmx.exe -a -t $1
+./dasmx140/Dasmx.exe -a ${in}
